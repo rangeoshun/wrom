7a5b8f4a54eb913192b076a35e309e059fd5ed58
Abel Varga <rangeoshun@gmail.com>
2016-04-08 16:52:04 +0200

    cleanup

--- src/client/js/angular-all-time-high.js
+++ src/client/js/angular-all-time-high.js
@@ -1,6 +1,4 @@
 module.exports = function ( $scope ) {
-  $scope.athScore = [{"pa":1,"so":10,"nm":"RANGE"},{"pa":2,"so":100000,"nm":"RANGEASDASDASDAS"}];
-
   $scope.$on('athUpdate', function ( ev, scores ) {
     $scope.athScores = scores;
   });
