09ace3372799f4dcb161cf2f330f713e3fc00abe
Abel Varga <rangeoshun@gmail.com>
2016-04-19 09:42:42 +0200

    Name setup fix

--- src/client/js/setup.js
+++ src/client/js/setup.js
@@ -6,59 +6,34 @@ String.prototype.replaceAt=function(index, character) {
 
 module.exports = function ( client ) {
   const nameInput = document.getElementById('name');
+  const legalName = /[a-zA-Z0-9]|-|_|'|/g;
+  const defaultName = 'UNNAMED';
 
   client.on('changeName', function ( name ) {
     nameInput.value = name;
   });
 
-  client.emit('changeName', client.globals.user.name || 'UNNAMED');
+  client.emit('changeName', client.globals.user.name || defaultName);
 
   function normalizeName ( name ) {
     if (!name) return '';
-    name = name.slice(0, 15);
-    for (let i = 0; i < name.length; i++) {
-      const code = name.charCodeAt(i);
-      if (code > 125) {
-        name.replaceAt(i, '_');
-        console.log(code, name)
-      }
-    }
-    return name;
+    return name.slice(0, 15).match(legalName).join('');
   }
 
   function handleNameChange ( ev ) {
     if (client.state !== 'setup') return;
-    ev.preventDefault();
 
     let value = ev.target.value || '';
-    const code = ev.keyCode;
-
-    if (code < 125) {
-
-      if (code !== 8) {
-        value += String.fromCharCode(code);
-      } else if (code === 13) {
-        nameInput.blur();
-        client.emit('goPlay');
-      } else {
-        value = value.slice(0, value.length - 1);
-      }
-
-    } else if (code === 189) {
-      value += '-';
-    } else if (code === 192) {
-      value += '_';
-    }
 
     value = normalizeName(value);
     client.emit('changeName', value);
+
+    if (ev.keyCode === 13) {
+      client.emit('goPlay');
+    }
   }
 
-  nameInput.addEventListener('keydown', function ( ev ) {
-    if (client.state !== 'setup') return;
-    ev.preventDefault();
-  });
-
+  nameInput.addEventListener('keydown', handleNameChange);
   nameInput.addEventListener('keyup', handleNameChange);
 
   document.getElementById('goPlay').addEventListener('click', function () {
